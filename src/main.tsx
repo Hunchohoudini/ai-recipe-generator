41961574111e4c5453b16b140742670d60eb8f06
Brent Camp <brentcamp20@outlook.com>
2025-11-27 16:09:22 -0800

    connect to bedrock

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "./App.jsx";
 import "./index.css";
-
-import { Amplify } from "aws-amplify";
-import outputs from "../amplify_outputs.json";
-
-import "@aws-amplify/ui-react/styles.css";
 import { Authenticator } from "@aws-amplify/ui-react";
 
-Amplify.configure(outputs);
-
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Authenticator>
       <App />
     </Authenticator>
   </React.StrictMode>
 );
-
